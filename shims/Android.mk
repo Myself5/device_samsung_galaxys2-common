f347d012cbbd0cc5fabe5125696ef1d035b67497
GreekDragon <alextsanisbadlorg@gmail.com>
2016-10-28 19:18:32 +0000

    galaxys2-common: Organize shim files
    Change-Id: Ic247c1f694102e081db906eeeb3221223b5ff09c

--- shims/Android.mk
+++ shims/Android.mk
@@ -12,6 +12,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# GPS-shim
+
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := gps.c
+
+LOCAL_SHARED_LIBRARIES := libcutils liblog
+
+LOCAL_MODULE := gps.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+include $(BUILD_SHARED_LIBRARY)
+
+# libsecril-shim
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	secril-shim.c
+	
+LOCAL_MODULE:= libsecril-shim
+LOCAL_SHARED_LIBRARIES := liblog libbinder
+
+include $(BUILD_SHARED_LIBRARY)
+
+# libshim_ril
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    samsung_ril.cpp
+
+LOCAL_SHARED_LIBRARIES := libbinder
+LOCAL_MODULE := libshim_ril
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
+
+# Thermal_shim
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := thermal.c
+
+LOCAL_MODULE := libshims_thermal
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
