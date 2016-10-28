62992d2e16b1c74bf42f8323cbc5552068b1bfdb
GreekDragon <alextsanisbadlorg@gmail.com>
2016-10-28 12:10:47 +0000

    galaxys2-common: shim ril & move
    Change-Id: Ia012c87af109c5c828645a42fd895c3cea257143

--- shims/Android.mk
+++ shims/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,15 +14,4 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    samsung_ril.cpp
-
-LOCAL_SHARED_LIBRARIES := libbinder
-
-LOCAL_MODULE := libsamsung_symbols
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
-include $(BUILD_SHARED_LIBRARY)
+include $(call all-makefiles-under,$(LOCAL_PATH))
